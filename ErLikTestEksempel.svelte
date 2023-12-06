eb4fa62fc753677341a55b86e62bc2ca374bfad2
Eirik <89630669+potat11@users.noreply.github.com>
2023-12-06 20:23:49 +0100

    Eksempelkode for erlik modulen

---
+++ ErLikTestEksempel.svelte
@@ -0,0 +1,22 @@
+<script>
+   import ErLikTest from "./ErLikTest.svelte"
+
+    let gjoerForbokstavStor = (streng) => {
+        let foersteBokstav = streng[0];
+        let resten = streng.slice(1);
+        return foersteBokstav.toUpperCase() + resten;
+    }
+</script>
+
+<!--Legg merke til arrayet i inndataene.-->
+<ErLikTest
+   inndata={["liten"]}
+   forventetUtdata={"Liten"}
+   funksjon={gjoerForbokstavStor}
+/>
+
+<ErLikTest
+   inndata={["liten"]}
+   forventetUtdata={"Liten"}
+   funksjon={gjoerForbokstavStor}
+/>
